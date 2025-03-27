c8780231c349e0c463d031f4a3c85d5348ceda11
Max <maxmilian.janda@gmail.com>
2025-03-27 20:11:22 +0100

    SetUpDesktop

--- fill.js
+++ fill.js
@@ -188,24 +188,3 @@ const closeMessage = document.getElementById("close");
 closeMessage.addEventListener("click", () => {
   draggableWindow.style.display = "none";
 });
-
-// ! Level status making
-
-const levelOne = document.getElementById("levelOne");
-const levelTwo = document.getElementById("levelTwo");
-const levelThree = document.getElementById("levelThree");
-
-let controller = 1;
-
-if (controller == 0) {
-  // level 1
-  levelTwo.style.color = "rgba(255, 255, 255, 0.2)";
-  levelThree.style.color = "rgba(255, 255, 255, 0.2)";
-} else if (controller == 1) {
-  // level 2
-  levelTwo.style.color = "rgb(255, 255, 255)";
-  levelThree.style.color = "rgba(255, 255, 255, 0.2)";
-} else {
-  levelThree.style.color = "rgb(255, 255, 255)";
-  // level 3
-}
